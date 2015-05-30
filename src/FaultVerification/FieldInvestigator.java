b54ef21a968961d0ca5ce07aef9db9fd115c380b
johnnylekwalo <lljleseyane@gmail.com>
2015-05-30 17:20:51 +0200

    Update FieldInvestigator.java

--- src/FaultVerification/FieldInvestigator.java
+++ src/FaultVerification/FieldInvestigator.java
@@ -6,14 +6,31 @@ public class FieldInvestigator {
 
 private Geolocation geolocation;
 private String areaOfJurisdiction;
+private GoogleMap googleMap;
 
 public FieldInvetigator(Geolocation geolocation,String areaOfJurisdiction){
   	this.areaOfJurisdiction = areaOfJurisdiction;
   	this.geolocation = areaOfJurisdiction;
 }
 
+public String getAreaOfJurisdiction() {
+  return this.areaOfJurisdiction;
+}
+
+public void setAreaOfJurisdiction(Geolocation geolocation) {
+  this.geolocation = geolocation;
+}
+
+public String getGoogleMap() {
+  return this.googleMap;
+}
+
+public void setGoogleMap(GoogleMap googleMap) {
+  this.googleMap = googleMap;
+}
+
 public Geolocation getGeolocation() {
-  return geolocation;
+  return this.geolocation;
 }
 
 public void setGeolocation(Geolocation geolocation) {
@@ -22,6 +39,18 @@ public void setGeolocation(Geolocation geolocation) {
 
 public void verifyFault(Fault fault) {
 		fault.setStatus("verified");
+}
+
+public void routeToFaults(String areaOfJurisdiction){
+	
+}
+
+public void routeToFaults(ArrayList<Fault> FaultList){
+	
+}
+
+public ArrayList<Fault> prioritizeFaults(ArrayList<Fault> FaultList){
+	
 }
 
 }
