220c894d586a8d476f7471c4f453afa828c6a539
adsteven <adsteven@192.168.0.9>
2015-05-30 16:31:22 +0200

    Need to fix bill of materials list

--- src/FaultRepair/AnnotatedValidWorkOrder.java
+++ src/FaultRepair/AnnotatedValidWorkOrder.java
@@ -2,18 +2,27 @@ package FaultRepair;
 
 public class AnnotatedValidWorkOrder extends WorkOrder{
 	private String _faultImage;
+	private String _faultAnnotation;
 	
-	
-	public AnnotatedValidWorkOrder(iFault fault,String imageName){
+	public AnnotatedValidWorkOrder(iFault fault,String imageName,String annotation){
 		super(fault);
 		setImage(imageName);
+		setFaultAnnotation(annotation);
 	}
 
+	private void setFaultAnnotation(String faultAnnotation){
+		_faultAnnotation = faultAnnotation;
+	}
+	
 	private void setImage(String imageName){
 		_faultImage = imageName;
 	}
 	
-	public String getImage(){
+	public String getFaultImage(){
 		return _faultImage;
+	}
+	
+	public String getFaultAnnotation(){
+		return _faultAnnotation;
 	}
 }
--- test/FaultRepair/WorkOrderTest.java
+++ test/FaultRepair/WorkOrderTest.java
@@ -5,6 +5,8 @@ package FaultRepair;
 import java.util.Date;
 import java.util.List;*/
 
+import java.awt.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
@@ -14,8 +16,9 @@ import FaultRepair.WorkOrder;
 public class WorkOrderTest {
 
 	private iFault _validTestFault,_invalidTestFault;
+	private iFault _validFaultWithGeolocation,_validFaultWithGeolocationAndNoWorkOrder;
 	private WorkOrder _validWorkOrder,_invalidWorkOrder;
-	private iFault _validFaultWithGeolocation;
+	
 	
 	@Before
 	public void upFront(){
@@ -26,22 +29,26 @@ public class WorkOrderTest {
 		_validFaultWithGeolocation = new iFault(true);
 		_validFaultWithGeolocation.validGeoLocation(true);
 		
+		_validFaultWithGeolocationAndNoWorkOrder = new iFault(true);
+		_validFaultWithGeolocationAndNoWorkOrder.validGeoLocation(true);
+		_validFaultWithGeolocationAndNoWorkOrder.hasWorkOrder(false);
+		
 		_validWorkOrder = new WorkOrder(_validTestFault);
 		_invalidWorkOrder = new WorkOrder(_invalidTestFault);
 	}
 	
 	@Test//Steven
-	public void testWorkOrderCreatedNoStatusForInvalidGeoLocation(){
+	public void testWorkOrderCreatedNoStatusForInvalidGeoLocationTest(){
 		Assert.assertTrue(_validWorkOrder.getStatus() == WorkOrder.STATUS.NOSTATUS);
 	}
 	
 	@Test//Steven
-	public void testWorkOrderNotCreatedOnInvalidFault(){ 
+	public void testWorkOrderNotCreatedOnInvalidFaultTest(){ 
 		Assert.assertTrue(_invalidWorkOrder.getStatus() == WorkOrder.STATUS.NOSTATUS);
 	}
 	
 	@Test//Steven
-	public void workOrderCreatedForFaultsVisibileOnMap(){
+	public void workOrderCreatedForFaultsVisibileOnMapTest(){
 		WorkOrder validWorkOrder = new WorkOrder(_validFaultWithGeolocation);
 		
 		Assert.assertTrue(validWorkOrder.getStatus() == WorkOrder.STATUS.ISSUED);
@@ -49,20 +56,27 @@ public class WorkOrderTest {
 	
 	@Test//Steven
 	public void workOrderCreatedProvidedThereIsNoOutstandingWorkOrder(){
-		_validFaultWithGeolocation.hasWorkOrder(false);
-		WorkOrder validWorkOrder = new WorkOrder(_validFaultWithGeolocation);
-		
+		WorkOrder validWorkOrder = new WorkOrder(_validFaultWithGeolocationAndNoWorkOrder);
+
 		Assert.assertTrue(validWorkOrder.getStatus() ==WorkOrder.STATUS.ISSUED);
 	}
 	
 	@Test//Steven
-	public void validWorkOrderCreationAcceptsAnnotation(){
-		_validFaultWithGeolocation.hasWorkOrder(false); 
-		AnnotatedValidWorkOrder annotatedValidWorkOrder = new AnnotatedValidWorkOrder(_validFaultWithGeolocation,"fault.png");
+	public void validWorkOrderCreationAcceptsAnnotationTest(){
+		WorkOrder annotatedValidWorkOrder = new AnnotatedValidWorkOrder(_validFaultWithGeolocationAndNoWorkOrder,"fault.png","Some Annotation");
 		
 		Assert.assertTrue(annotatedValidWorkOrder.getStatus() == WorkOrder.STATUS.ISSUED);
 		
-		String imageCheckString = annotatedValidWorkOrder.getImage();
+		String imageCheckString = annotatedValidWorkOrder.getFaultImage();
 		Assert.assertTrue(imageCheckString  instanceof String);
+	}
+	
+	@Test//Steven
+	public void billOfMaterialsIsCreatedForNatureOfWorkSpecifiedTest(){
+		WorkOrder validWorkOrder = new WorkOrder(_validFaultWithGeolocationAndNoWorkOrder);
+		//BOMaterials  = <>Materials;
+		BillOfMaterials testMaterialsList = validWorkOrder.generateBOMaterials(iFault.FAULTTYPE.POTHOLE);
+		List checkList = testMaterialsList.getMaterialsList();
+		//Assert.assertTrue(checkList.size()> 0);
 	}
 }
